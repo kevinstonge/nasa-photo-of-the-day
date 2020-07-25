14374567a159dfd84076114f5b169d5ff48acf22
Kevin St.Onge <kevinstonge@gmail.com>
2020-07-25 09:08:58 -0400

    getting started

---
+++ src/components/APOD.js
@@ -0,0 +1,9 @@
+import React from "react";
+const APOD = props => {
+    return(
+        <div>
+            container
+        </div>
+    )
+}
+export default APOD;
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+const Header = props => {
+    return(
+        <div>
+            <h1>NASA APOD</h1>
+        </div>
+    )
+}
+export default Header;
