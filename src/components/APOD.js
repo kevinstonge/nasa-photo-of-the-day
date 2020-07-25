02c7327066cf0c095298289218e3eb39b284c8c4
Kevin St.Onge <kevinstonge@gmail.com>
2020-07-25 10:34:14 -0400

    picture and title plus some sass

--- src/components/APOD.js
+++ src/components/APOD.js
@@ -1,8 +1,15 @@
 import React from "react";
+import "./APOD.sass"
 const APOD = props => {
+    console.log(props);
     return(
-        <div>
-            container
+        <div className="apod_container">
+            <div className="image_container">
+                <img src={props.data.data.url} alt={props.data.data.title}/>
+            </div>
+            <div className="title_container">
+                    <h2>{props.data.data.title}</h2>
+            </div>
         </div>
     )
 }
