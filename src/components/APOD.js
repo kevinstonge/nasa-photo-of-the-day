8e038e47543c6a8c32e2c2441dcac15a9c5343fc
Kevin St.Onge <kevinstonge@gmail.com>
2020-07-27 10:09:57 -0400

    mostly scss

--- src/components/APOD.js
+++ src/components/APOD.js
@@ -3,9 +3,20 @@ import "./APOD.scss"
 const APOD = props => {
     console.log(props);
     return(
+        // date must be between Jun 16, 1995 and {today}
+        //a couple dates have no image, so build missing/error handler
+        //no image returns "code":400
         <div className="apod_container">
+            {/* break this out into a component */}
+            <div className="apod_controls">
+                <p className="apod_control">select date</p>
+                <p className="apod_control">></p>
+            </div>
+            {/* end component */}
             <div className="image_container">
+                {/* TODO: replace static image reference with props reference for final build */}
                 {/* <img src={props.data.data.url} alt={props.data.data.title}/> */}
+                <img src="169A2911Dai1024.jpg" alt={props.data.data.title}></img>
                 <div className="title_container">
                     <h2>{props.data.data.title}</h2>
                 </div>
