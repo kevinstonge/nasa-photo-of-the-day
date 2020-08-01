fd126d24b6be1814972f98194d1b77bcbb6cf763
Kevin St.Onge <kevinstonge@gmail.com>
2020-08-01 07:33:40 -0400

    stretch: used Emotion for Background.js

--- src/components/Background.js
+++ src/components/Background.js
@@ -1,28 +1,46 @@
-import React, { memo } from "react";
-import "./Background.scss";
+import { memo } from "react";
+import { css, jsx, keyframes } from "@emotion/core";
+/** @jsx jsx */
 
 const Background = memo(() => {
-    //stars is array of dom elements with random positions
-    const stars = new Array(150).fill(0).map(s=>{
-        return({
-        top: `${Math.random()*100}vh`,
-        left: `${Math.random()*100}vw`,
-        animationDelay: `-${Math.floor(Math.random()*30)}s`,
-        })
-    });
-    return(
-        <div>
-        {stars.map((e,i)=>
-            <div 
-                className="star"
-                key={`star${i}`}
-                style={{top:e.top, left:e.left, animationDelay:e.animationDelay}}    
-            >
-
-            </div>
-        )
-        }   
-        </div>
-    )
+  const colors = new Array(200)
+    .fill(0)
+    .map((e) => `hsl(${Math.floor(Math.random() * 360)},80%,90%)`);
+  const pulse = (color) => keyframes`0% {
+        box-shadow: 0 0 0.01px 0.01px ${color};
+      }
+      50% {
+        box-shadow: 0 0 7px 3px ${color};
+      }
+      100% {
+        box-shadow: 0 0 0.01px 0.01px ${color};
+      }`;
+  return (
+    <div>
+      {colors.map((e, i) => (
+        <div
+          css={css`
+            position: fixed;
+            top: ${Math.random() * 100}vh;
+            left: ${Math.random() * 100}vw;
+            background-color: ${e};
+            border-radius: 50%;
+            width: 1px;
+            height: 1px;
+            z-index: -10;
+            box-shadow: 0 0 0.1rem 0.1rem ${e};
+            animation-name: ${pulse(e)};
+            animation-duration: 30s;
+            animation-delay: -${Math.floor(Math.random() * 30)}s;
+            animation-iteration-count: infinite;
+            animation-timing-function: ease-in-out;
+            -webkit-backface-visibility: hidden;
+            backface-visibility: hidden;
+          `}
+          key={`star${i}`}
+        ></div>
+      ))}
+    </div>
+  );
 });
 export default Background;
